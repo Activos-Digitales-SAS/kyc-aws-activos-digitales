470a103adae3f6221921283b13166a3bf23fa1ab
DevJohanR <desarrollador7777@gmail.com>
2024-06-12 15:55:33 -0500

    AWS carga de archivos primera fase completada

--- server/photos.routes.js
+++ server/photos.routes.js
@@ -1,13 +1,15 @@
 const { Router } = require('express')
-
+const { uploadFile } = require('./s3.js')
 const router = Router()
 
 router.get('/', (req,res)=> res.send('welcome to server'))
 
-router.post('/upload', (req,res)=> {
+router.post('/upload', async (req,res)=> {
 
-    console.log(req.files)
+    console.log(req.files['photo'])
+  const result = await uploadFile(req.files['photo'])
     
+   console.log(result)
     res.send('archivo subido')
 })
 
